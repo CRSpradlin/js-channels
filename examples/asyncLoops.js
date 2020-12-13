39137a620349bc9494ec7355c369d93640ce9885
crspradlin <mail@crspradlin.org>
2020-12-13 11:23:03 -0500

    updated example and pushed 1.0.1 version to npm

--- examples/asyncLoops.js
+++ examples/asyncLoops.js
@@ -1,26 +1,26 @@
-import Channel from '../index.js'
+const Channel = require('../index').Channel
 
-let ch = new Channel()
+const ch = new Channel()
 
 // Sender function to send via the channel
-let sender = async () => {
-    for(let i=0; i<1000; i++){
-        // await key word can be placed in front of the below command
-        // to sync the two async functions
-        ch.send(i)
-    }
+const sender = async () => {
+  for (let i = 0; i < 1000; i++) {
+    // await key word can be placed in front of the below command
+    // to sync the two async functions
+    ch.send(i)
+  }
 }
 
 // Receiver function to receive the channel messages
-let receiver = async () => {
-    for(let i=0; i<1000; i++){
-        console.log(await ch.receive())
-    }
+const receiver = async () => {
+  for (let i = 0; i < 1000; i++) {
+    console.log(await ch.receive())
+  }
 }
 
 // Main function runs the above two async functions
-let main = () => {
-    receiver()
-    sender()
+const main = () => {
+  receiver()
+  sender()
 }
 main()
